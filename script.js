d23ec5e52b0d32dcc426bc97a53756786d25940e
tapo02 <163664716+tapo02@users.noreply.github.com>
2025-03-10 21:05:24 +0100

    First draft

---
+++ script.js
@@ -0,0 +1,23 @@
+const facts = [
+    "I hate it when people try to pass you at the top of an escalator. Like are you trying to make getting off as hard as possible??",
+    "I am a very messy person. Somehow, however, I cannot work if the space around me is not tidy. It's like I'm a completely different person when working.",
+    "Whenever I discover new songs I like, I listen to them nonstop until I don't like them anymore. Self-sabotage at its finest.",
+    "My great-grandfather was a composer.",
+    "I once was left behind by my teacher during a school ski course. I had to join another teacher's group to not get lost completely.",
+    "I've known 'Wicked' inside out years before the movie came out. Since then it's become my whole personality (again).",
+    "I've completed a Dutch-language summer course in Hungary. Talk about random."
+];
+
+function select_random_fact() {
+    const random_fact = facts[Math.floor(Math.random() * facts.length)]
+    document.getElementById("fact").textContent = random_fact;
+    document.getElementById("hide_button").style.display = "inline-block";
+}
+
+function hide_fact() {
+    document.getElementById("fact").textContent = "";
+    document.getElementById("hide_button").style.display = "none";
+}
+
+document.getElementById("fact_generator").addEventListener("click", select_random_fact);
+document.getElementById("hide_button").addEventListener("click", hide_fact);
